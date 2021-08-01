27b6eff40c99f34d815b07562cbbf8033e5c6c0e
alfu32 <ioan.alferaru@gmail.com>
2021-08-01 10:55:41 +0200

    #3:kanban board

--- src/Kanban.js
+++ src/Kanban.js
@@ -1,21 +1,49 @@
 import React, { useState, useEffect } from 'react';
+import { Button } from 'primereact/button';
 import './Kanban.css';
 
+let sequence = 0;
+const createItem = (state) => {
+  return ({
+    state,
+    id:sequence++,
+    title:"new task",
+    description:"task description",
+  })
+}
 export const Kanban = (props) => {
-  const [boardId,setBoardId] = useState(0);
+  const [taskList,setTaskList] = useState([]);
   const states = props.workflow.reduce((s,i)=>{
     s[i.from]=true;
     s[i.to]=true;
     return s;
-  },{})
+  },{});
+  const createTask = (state) => {
+    setTaskList([...taskList,createItem(state)])
+  }
   return (<>
     <h4>{props.title} Board</h4>
     <div className="lanes">
     {Object.keys(states).map(state => {
       return (<div className="lane">
         <h4>{state}</h4>
+        <Button onClick={() => {
+          createTask(state);
+        }}>+</Button>
         <div className="list">
-
+          {
+            taskList.filter(
+              task => task.state === state
+            )
+            .map(
+              (task) => {
+                return (<div className="task">
+                  <h5>#{task.id} {task.title}</h5>
+                  <div>{task.description}</div>
+                </div>)
+              }
+            )
+          }
         </div>
       </div>)
     })}
