74be47e098a30653f50c69ad6f7caddf3dbeb240
alfu32 <ioan.alferaru@gmail.com>
2021-08-01 08:31:29 +0200

    #2: todolist

--- src/TodoList.js
+++ src/TodoList.js
@@ -6,7 +6,7 @@ let sequence = 1;
 const getNewItem = () => {
   return {
     id:sequence++,
-    checked: false,
+    checked: "todo",
     text: 'new item'
   };
 };
@@ -14,8 +14,8 @@ const btnCssStyle = 'p-button-text';
 
 export const TodoList = props => {
   const [list, setList] = useState([]);
-  const setChecked = item => {
-    item.checked = !item.checked;
+  const setChecked = (item,status) => {
+    item.checked = status;
     setList([...list]);
   };
   const setItemText = (item,e) => {
@@ -34,35 +34,68 @@ export const TodoList = props => {
       >
         +
       </Button>
+      <h4>Todo</h4>
       <ul>
         {list
-        .filter(it => it.checked===false)
+        .filter(it => it.checked==="todo")
         .map(item => {
           return (
             <li>
               <span>{item.id}.</span>
-              <Checkbox
-                onChange={e => {
-                  setChecked(item);
-                }}
-                checked={item.checked}
-              />
               {/**<span>{item.text}</span>**/}
               <input type="text" value={item.text} onChange={(e) => {
                 setItemText(item,e);
               }}/>
+              <Button
+                className={btnCssStyle}
+                onClick={() => {
+                  setChecked(item,"progress");
+                }}
+              >
+                Start
+              </Button>
             </li>
           );
         })}
       </ul>
+      <h4>Progress</h4>
       <ul>
         {list
-        .filter(it => it.checked===true)
+        .filter(it => it.checked==="progress")
         .map(item => {
           return (
             <li>
               <span>{item.id}.</span>
               <span>{item.text}</span>
+              <Button
+                className={btnCssStyle}
+                onClick={() => {
+                  setChecked(item,"done");
+                }}
+              >
+                Done
+              </Button>
+            </li>
+          );
+        })}
+      </ul>
+      <h4>Done</h4>
+      <ul>
+        {list
+        .filter(it => it.checked==="done")
+        .map(item => {
+          return (
+            <li>
+              <span>{item.id}.</span>
+              <span>{item.text}</span>
+              <Button
+                className={btnCssStyle}
+                onClick={() => {
+                  setChecked(item,"todo");
+                }}
+              >
+                Todo
+              </Button>
             </li>
           );
         })}
