076b135ee9ec589bb52f0a69d3228ff6ba4e9508
alfu32 <ioan.alferaru@gmail.com>
2021-08-01 11:02:31 +0200

    #3:kanban board:#4create tasks

--- src/Kanban.js
+++ src/Kanban.js
@@ -3,50 +3,60 @@ import { Button } from 'primereact/button';
 import './Kanban.css';
 
 let sequence = 0;
-const createItem = (state) => {
-  return ({
+const createItem = state => {
+  return {
     state,
-    id:sequence++,
-    title:"new task",
-    description:"task description",
-  })
-}
-export const Kanban = (props) => {
-  const [taskList,setTaskList] = useState([]);
-  const states = props.workflow.reduce((s,i)=>{
-    s[i.from]=true;
-    s[i.to]=true;
+    id: sequence++,
+    title: 'new task',
+    description: 'task description',
+    createdDate: new Date()
+  };
+};
+const stlBtnAdd = 'p-button-rounded p-button-text p-button-sm';
+
+export const Kanban = props => {
+  const [taskList, setTaskList] = useState([]);
+  const states = props.workflow.reduce((s, i) => {
+    s[i.from] = true;
+    s[i.to] = true;
     return s;
-  },{});
-  const createTask = (state) => {
-    setTaskList([...taskList,createItem(state)])
-  }
-  return (<>
-    <h4>{props.title} Board</h4>
-    <div className="lanes">
-    {Object.keys(states).map(state => {
-      return (<div className="lane">
-        <h4>{state}</h4>
-        <Button onClick={() => {
-          createTask(state);
-        }}>+</Button>
-        <div className="list">
-          {
-            taskList.filter(
-              task => task.state === state
-            )
-            .map(
-              (task) => {
-                return (<div className="task">
-                  <h5>#{task.id} {task.title}</h5>
-                  <div>{task.description}</div>
-                </div>)
-              }
-            )
-          }
-        </div>
-      </div>)
-    })}
-    </div>
-  </>)
-}
+  }, {});
+  const createTask = state => {
+    setTaskList([...taskList, createItem(state)]);
+  };
+  return (
+    <>
+      <h4>{props.title} Board</h4>
+      <div className="lanes">
+        {Object.keys(states).map(state => {
+          return (
+            <div className="lane">
+              <h4>{state}</h4>
+              <Button
+                icon="pi pi-plus"
+                className={stlBtnAdd}
+                onClick={() => {
+                  createTask(state);
+                }}
+              />
+              <div className="list">
+                {taskList
+                  .filter(task => task.state === state)
+                  .map(task => {
+                    return (
+                      <div className="task">
+                        <h5>
+                          #{task.id} {task.title}
+                        </h5>
+                        <div>{task.description}</div>
+                      </div>
+                    );
+                  })}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
