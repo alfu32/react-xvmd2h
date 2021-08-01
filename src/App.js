a238634150ce8eb44683be7ea654dc8ad6cfe4c1
alfu32 <ioan.alferaru@gmail.com>
2021-08-01 08:22:19 +0200

    #2: todolist

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './style.css';
 import {Counter} from './Counter';
+import {TodoList} from './TodoList';
 
 export default function App() {
   return (
     <div>
       <Counter name='coucou'/>
       <Counter name='coucou2'/>
+      <TodoList/>
     </div>
   );
 }
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,13 +1,72 @@
 import React, { useState, useEffect } from 'react';
+import { Button } from 'primereact/button';
+import { Checkbox } from 'primereact/checkbox';
 
-export const TodoList = (props) => {
-  const [list,setList] = useState([]);
-  return (<ul>{
-    list.map(item => {
-      
-      return (<li>
+let sequence = 1;
+const getNewItem = () => {
+  return {
+    id:sequence++,
+    checked: false,
+    text: 'new item'
+  };
+};
+const btnCssStyle = 'p-button-text';
 
-      </li>);
-    })
-  }</ul>);
-}
+export const TodoList = props => {
+  const [list, setList] = useState([]);
+  const setChecked = item => {
+    item.checked = !item.checked;
+    setList([...list]);
+  };
+  const setItemText = (item,e) => {
+    console.log(e)
+    item.text = e.target.value;
+    setList([...list]);
+  };
+  return (
+    <div>
+      <div>To Do List </div>
+      <Button
+        className={btnCssStyle}
+        onClick={() => {
+          setList([...list, getNewItem()]);
+        }}
+      >
+        +
+      </Button>
+      <ul>
+        {list
+        .filter(it => it.checked===false)
+        .map(item => {
+          return (
+            <li>
+              <span>{item.id}.</span>
+              <Checkbox
+                onChange={e => {
+                  setChecked(item);
+                }}
+                checked={item.checked}
+              />
+              {/**<span>{item.text}</span>**/}
+              <input type="text" value={item.text} onChange={(e) => {
+                setItemText(item,e);
+              }}/>
+            </li>
+          );
+        })}
+      </ul>
+      <ul>
+        {list
+        .filter(it => it.checked===true)
+        .map(item => {
+          return (
+            <li>
+              <span>{item.id}.</span>
+              <span>{item.text}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
