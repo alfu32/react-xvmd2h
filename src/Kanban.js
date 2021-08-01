8a498a11cfac5e684159425ea43078e77018699d
alfu32 <ioan.alferaru@gmail.com>
2021-08-01 09:38:01 +0200

    #3: kanban board

--- src/Kanban.js
+++ src/Kanban.js
@@ -2,5 +2,22 @@ import React, { useState, useEffect } from 'react';
 
 export const Kanban = (props) => {
   const [boardId,setBoardId] = useState(0);
-  return (<></>)
+  const states = props.states.reduce((s,i)=>{
+    s[i.from]=true;
+    s[i.to]=true;
+    return s;
+  },{})
+  return (<>
+    <h4>{props.title} Board</h4>
+    <div className="lanes">
+    {Object.keys(states).map(state => {
+      return (<div className="lane">
+        <h4>{state}</h4>
+        <div className="list">
+
+        </div>
+      </div>)
+    })}
+    </div>
+  </>)
 }
